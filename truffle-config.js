fef77dc107da48c5b3680e38d32f22ee3173e659
Dhruvin Parikh <imsilent93@gmail.com>
2021-03-22 22:00:16 -0400

    upgrade deps (#4)

--- truffle-config.js
+++ truffle-config.js
@@ -17,14 +17,14 @@
  * phrase from a file you've .gitignored so it doesn't accidentally become public.
  *
  */
-require('dotenv').config("./env")
-const ethers = require('ethers');
-const HDWalletProvider = require('truffle-hdwallet-provider');
+require("dotenv").config("./env");
+// const ethers = require("ethers");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
-const privateKey = process.env.PRIVATE_KEY;
+// const privateKey = process.env.PRIVATE_KEY;
 
 module.exports = {
   /**
@@ -52,40 +52,49 @@ module.exports = {
 
     // Another network with more advanced options...
     // advanced: {
-      // port: 8777,             // Custom port
-      // network_id: 1342,       // Custom network
-      // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-      // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-      // from: <address>,        // Account to send txs from (default: accounts[0])
-      // websockets: true        // Enable EventEmitter interface for web3 (default: false)
+    // port: 8777,             // Custom port
+    // network_id: 1342,       // Custom network
+    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+    // from: <address>,        // Account to send txs from (default: accounts[0])
+    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
     // },
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
-      provider: () => new HDWalletProvider(privateKey, (new ethers.providers.InfuraProvider("ropsten")).connection.url),
-      network_id: 3,       // Ropsten's id
-      from: process.env.OWNER_ADDRESS,
-      gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: `${process.env.MNEMONIC}`,
+          },
+          providerOrUrl: `https://ropsten.infura.io/v3/${process.env.INFURA_KEY}`,
+          numberOfAddresses: 1,
+          shareNonce: true,
+          derivationPath: "m/44’/60’/0’/0",
+        }),
+      network_id: 3, // Ropsten's id
     },
     rinkeby: {
-      provider: () => new HDWalletProvider(privateKey, (new ethers.providers.InfuraProvider("rinkeby")).connection.url),
-      network_id: 4,       // Rinkeby's id
-      from: process.env.OWNER_ADDRESS,
-      gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: `${process.env.MNEMONIC}`,
+          },
+          providerOrUrl: `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`,
+          numberOfAddresses: 1,
+          shareNonce: true,
+          derivationPath: "m/44'/60'/0'/0/",
+          pollingInterval: 8000
+        }),
+      network_id: 4, // Rinkeby's id
     },
 
     // Useful for private networks
     // private: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-      // network_id: 2111,   // This network is yours, in the cloud.
-      // production: true    // Treats this network as if it was a public net. (default: false)
+    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+    // network_id: 2111,   // This network is yours, in the cloud.
+    // production: true    // Treats this network as if it was a public net. (default: false)
     // }
   },
 
@@ -106,6 +115,6 @@ module.exports = {
       //  },
       //  evmVersion: "byzantium"
       // }
-    }
-  }
-}
+    },
+  },
+};
